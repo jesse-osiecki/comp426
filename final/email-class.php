3b8255590355045ebdce0eee78ac4c566717ccc5
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-12-07 23:47:27 -0500

    working on final proj

--- final/email-class.php
+++ final/email-class.php
@@ -129,7 +129,7 @@ class emailclass{
         if ($query) {
             foreach($query as $next=>$next_row) {
                 if(array_key_exists('id', $next_row)){
-                    $id_array[] = intval($next_row['id']);
+                    $id_array[] = $next_row['id'];
                 }
                 echo(json_encode($id_array));
                 //echo(json_encode($next) . " ||| " . json_encode($next_row));
