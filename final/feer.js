2c0126f5500f5719b1c106483432e53f7ee2a043
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-12-08 00:35:52 -0500

    working on final proj

--- final/feer.js
+++ final/feer.js
@@ -10,15 +10,15 @@ $(document).ready(function () {
 
     //////////////////////
     //What to do when the user hits submit
-    $('#submit').click(function( event ) {
-        //event.preventDefault();
-        ////Post to the test.php page and get content which has been returned in json format (<?php echo json_encode(array("name"=>"John","time"=>"2pm")); ?>).
-        //var posting = $.post( "submit.php", { func: "getAttr" }, function() {
-        //    //console.log( data.name ); // John
-        //    console.log( then ); // time to send
-        //}, "json");
-        alert("Submit");
-    });
+    //$('#submit').click(function( event ) {
+    //    event.preventDefault();
+    //    //Post to the test.php page and get content which has been returned in json format (<?php echo json_encode(array("name"=>"John","time"=>"2pm")); ?>).
+    //    var posting = $.post( "submit.php", { func: "getAttr" }, function() {
+    //        //console.log( data.name ); // John
+    //        console.log( then ); // time to send
+    //    }, "json");
+    //    alert("Submit");
+    //});
 
     //UPDATE the date
     var update_date = function() {
