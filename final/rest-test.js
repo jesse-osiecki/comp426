87704cdad5ddf8a49290aa9eb31b870999cc4de8
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-12-08 00:57:52 -0500

    working on final proj

--- final/rest-test.js
+++ final/rest-test.js
@@ -1,41 +1,16 @@
 $(document).ready(function () {
-	$('#restform').on('submit', form_submit_handler);
-	$('#clearbutton').on('click', function () {
-		$('#params input').each(function (i,e) {
-			$(e).val('');
-		    });
-	    });
-    });
+    $('#restform').on('submit', form_submit_handler);
+});
 
 var form_submit_handler = function (e) {
     e.preventDefault();
-    e.stopPropagation();
-
-    // Collect parameter name/value pairs as data
-    var data_pairs = {}
-    $('#params div').each(function (i, e) {
-	    var pname = $.trim($(e).find('input.pval').attr('name'));
-	    if (pname != "") {
-		data_pairs[$(e).find('input.pval').attr('name')] =
- 		    $(e).find('input.pval').val();
-	    }
-	});
-		
-    // Get URL from rest_url text input
-    //var ajax_url = $('#rest_url').val();
-    var ajax_url = '/email.php';
-
-    // Set up settings for AJAX call
-    var settings = {
-	type: 'POST',
-	data: data_pairs,
-	success: ajax_success_handler,
-	error: ajax_error_handler,
-	cache: false
-    }
-
-    // Make AJAX call
-    $.ajax(ajax_url, settings);
+    $.ajax(url_base + "/email.php",
+            {type: "POST",
+                dataType: "json",
+                data: $(this).serialize(),
+                success: ajax_success_handler,
+                error: ajax_error_handler,
+            });
 };
 
 var ajax_success_handler = function(data, textStatus, jqXHR) {
