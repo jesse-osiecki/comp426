ec6db64176908f46811e5000ecc66151a76ed12c
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-12-08 21:31:28 -0500

    working on final proj

--- final/cron.php
+++ final/cron.php
@@ -5,16 +5,15 @@ global $db;
 $now = time(); //get current unix time
 
 $query = $db->get_results("select * from email where scheduledtime<='$now'");
-//echo(json_encode($query));
 foreach($query as $key=>$row){
-    //echo(json_encode($row) . "\n");
     $to = $row->email_to;
     $subject = 'F.E.E.R Reminder Email'; //This is a defacto subject.... I forgot to add a subject variable everywhere else
     $message = $row->messagebody;
     $headers = 'From: ' . $row->email_from . "\r\n";
-    $headers = (strlen($row->cc) > 1) ? ($headers . 'Cc: ' . $row->email_cc . "\r\n") : $headers;
+    $headers = (strlen($row->email_cc) > 1) ? ($headers . 'Cc: ' . $row->email_cc . "\r\n") : $headers;
     $headers .= 'Reply-To: ' . $row->email_from . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
-    mail($to, $subject, $message, $headers);
+    $mail = mail($to, $subject, $message, $headers);
+    echo($mail . $row->id . "\n");
 }
 ?>
