68bcc9c9ae515765480afaf0d259ac5277d0e4e0
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-12-08 22:24:44 -0500

    working on final proj

--- final/login.php
+++ final/login.php
@@ -21,62 +21,62 @@
                 </ul>
                 <h1>The Friendly Everyday Email Robot</h1>
             </div>
-        </div>
-        <div class="row">
-            <h2> Login: </h2>
-            <form action="login-action.php" method="post" role="form" class="row">
-                <fieldset>
+            <div class="row">
+                <h2> Login: </h2>
+                <form action="login-action.php" method="post" role="form" class="row">
+                    <fieldset>
+                        <div class="col-xs-2">
+                            <label for="username">Username: </label>
+                            <input type="text" name="username" id="username" value="" class="form-control" placeholder="user"/>
+                        </div>
+                        <div class="col-xs-2">
+                            <label for="password">Password: </label>
+                            <input type="password" name="password" id="password" value="" class="form-control" />
+                        </div>
+                        <p>
+                        <div class="col-xs-2">
+                            <label for="remember">
+                                <input type="checkbox" name="remember" id="remember" value="1" class="form-control"/> Remember me
+                            </label>
+                        </div>
+                        </p>
+                    </fieldset>
                     <div class="col-xs-2">
-                        <label for="username">Username: </label>
-                        <input type="text" name="username" id="username" value="" class="form-control" placeholder="user"/>
+                        <input type="submit" value="Submit" class="btn btn-primary btn-xs"/>
                     </div>
+                </form>
+            </div>
+            <div class="row">
+                <h2> Create an Account: </h2>
+                <form action="create-user-action.php" method="post" role="form" class="row">
+                    <fieldset>
+                        <div class="col-xs-2">
+                            <label for="username">Username: </label>
+                            <input type="text" name="username" id="username" value="" class="form-control" placeholder="user"/>
+                        </div>
+                        <div class="col-xs-2">
+                            <label for="nicename">Full Name: </label>
+                            <input type="text" name="nicename" id="nicename" value="" class="form-control" placeholder="User U. McManWoman"/>
+                        </div>
+                        <div class="col-xs-2">
+                            <label for="email">Email: </label>
+                            <input type="text" name="email" id="email" value="" class="form-control" placeholder="email"/>
+                        </div>
+                        <div class="col-xs-2">
+                            <label for="password">Password: </label>
+                            <input type="password" name="password" id="password" value="" class="form-control" />
+                        </div>
+                        <div class="col-xs-2">
+                            <label for="trickquestion">Teacher's last name?: </label>
+                            <!-- Slow down the Spam bots, answer is Patel -->
+                            <input type="text" name="trickquestion" id="trickquestion" value="" class="form-control" />
+                        </div>
+                    </fieldset>
                     <div class="col-xs-2">
-                        <label for="password">Password: </label>
-                        <input type="password" name="password" id="password" value="" class="form-control" />
+                        <input type="submit" value="Submit" class="btn btn-primary btn-xs"/>
                     </div>
-                    <p>
-                    <div class="col-xs-2">
-                        <label for="remember">
-                            <input type="checkbox" name="remember" id="remember" value="1" class="form-control"/> Remember me
-                        </label>
-                    </div>
-                    </p>
-                </fieldset>
-                <div class="col-xs-2">
-                    <input type="submit" value="Submit" class="btn btn-primary btn-xs"/>
-                </div>
-            </form>
-        </div>
-        <div class="row">
-            <h2> Create an Account: </h2>
-            <form action="create-user-action.php" method="post" role="form" class="row">
-                <fieldset>
-                    <div class="col-xs-2">
-                        <label for="username">Username: </label>
-                        <input type="text" name="username" id="username" value="" class="form-control" placeholder="user"/>
-                    </div>
-                    <div class="col-xs-2">
-                        <label for="nicename">Full Name: </label>
-                        <input type="text" name="nicename" id="nicename" value="" class="form-control" placeholder="User U. McManWoman"/>
-                    </div>
-                    <div class="col-xs-2">
-                        <label for="email">Email: </label>
-                        <input type="text" name="email" id="email" value="" class="form-control" placeholder="email"/>
-                    </div>
-                    <div class="col-xs-2">
-                        <label for="password">Password: </label>
-                        <input type="password" name="password" id="password" value="" class="form-control" />
-                    </div>
-                    <div class="col-xs-2">
-                        <label for="trickquestion">Teacher's last name?: </label>
-                        <!-- Slow down the Spam bots, answer is Patel -->
-                        <input type="text" name="trickquestion" id="trickquestion" value="" class="form-control" />
-                    </div>
-                </fieldset>
-                <div class="col-xs-2">
-                    <input type="submit" value="Submit" class="btn btn-primary btn-xs"/>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </body>
 </html>
