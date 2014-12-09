dcd0c528158294efcd01f2273dbb47665e0debac
Jesse Osiecki <jesse.osiecki@gmail.com>
2014-12-08 21:35:43 -0500

    working on final proj

--- final/cron.php
+++ final/cron.php
@@ -1,7 +1,6 @@
 <?php
 include_once 'db.php';
 global $db;
-$boolarray = Array(false => 'false', true => 'true');
 $now = time(); //get current unix time
 
 $query = $db->get_results("select * from email where scheduledtime<='$now'");
@@ -14,6 +13,7 @@ foreach($query as $key=>$row){
     $headers .= 'Reply-To: ' . $row->email_from . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
     $mail = mail($to, $subject, $message, $headers);
-    echo($boolarray[$mail] . $row->id . "\n");
+    echo $mail ? "true " : "false ";
+    echo($row->id . "\n");
 }
 ?>
